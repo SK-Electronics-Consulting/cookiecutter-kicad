051601accb8e24babe8d7e993f7b4ceb3f82cc85
Seth Kazarians <seth.kazarians@gmail.com>
2021-09-13 09:50:54 -0700

    Added hooks for creating and using local repo credentials.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,11 @@ import subprocess
 # Initialize the repo
 subprocess.call(["git", "init"])
 
+if {{cookiecutter.use_local_repo_credentials}}:
+    subprocess.call(["git", "config", "--local", "user.name", "{{cookiecutter.Name}}"])
+    subprocess.call(["git", "config", "--local", "user.email", "{{cookiecutter.email}}"])
+
+
 # Add the basics
 subprocess.call(["git", "add", ".gitignore", "PCB/*", "config.kiplot.yaml"])
 subprocess.call(["git", "commit", "-m", "\"Initial commit of empty project and git-specific files\""])
