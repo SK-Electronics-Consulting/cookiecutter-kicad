545840d6a9a7601ee3c4c949ccd44f2a9ed4c2dd
Seth Kazarians <seth@kazarians.dev>
2023-08-15 13:47:37 -0700

    Update user name/e-mail to less ambiguous names

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,10 +4,10 @@ import subprocess
 subprocess.call(["git", "init"])
 
 if {{cookiecutter.use_local_repo_credentials}}:
-    subprocess.call(["git", "config", "--local", "user.name", "{{cookiecutter.Name}}"])
-    subprocess.call(["git", "config", "--local", "user.email", "{{cookiecutter.email}}"])
+    subprocess.call(["git", "config", "--local", "user.name", "{{cookiecutter.User_Name}}"])
+    subprocess.call(["git", "config", "--local", "user.email", "{{cookiecutter.User_email}}"])
 
 
 # Add the basics
-subprocess.call(["git", "add", ".gitignore", "PCB/*", "config.kibot.yaml"])
+subprocess.call(["git", "add", ".gitignore", "PCB/*", "config.kibot.yaml", ".github/*"])
 subprocess.call(["git", "commit", "-m", "\"Initial commit of empty project and git-specific files\""])
